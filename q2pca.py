bdc3944bf8cb3056bc868ef31b14dd1fb5abaa28
Aui Mendoza <auimendoza@gmail.com>
2018-10-29 21:12:46 -0400

    q2 pca ok

---
+++ q2pca.py
@@ -0,0 +1,55 @@
+from __future__ import print_function
+
+from sklearn.decomposition import PCA
+from sklearn.metrics import mean_squared_error
+import numpy as np
+from common import *
+
+seed=42
+method='PCA'
+
+def reduceDim(label, method, X, n_components, reconstructimages=False, seed=seed):
+    print("doing %s..." % (method))
+    mse = []
+    firstimages = []
+    model = None
+    n = -1
+    Xt = None
+
+    for n in n_components:
+      model = PCA(n_components=n, random_state=seed)
+      Xt = model.fit_transform(X)
+      Xr = model.inverse_transform(Xt)
+      mse.append(mean_squared_error(X, Xr))
+      firstimages.append(Xr[0,:])
+
+    print("done. plotting...")
+
+    plot_re(label, method, mse, n_components)
+    if reconstructimages:
+      firstimages.insert(0, np.array(X.iloc[0,:]))
+      plot_first_images(firstimages, n_components, method, label)
+
+    ver = model.explained_variance_ratio_
+    evr = np.cumsum(model.explained_variance_ratio_)
+    crange = np.where(evr > 0.95)   
+    usen = X.shape[1]
+    if len(crange[0]) > 0:
+        usen = min(crange[0])+1
+        print ("minimum # of components with variance explained > 0.95: %d" % (usen))
+    biplot(label, method, Xt[:,0:2],np.transpose(model.components_[0:2, :]), X.columns.tolist())
+    plot_scree(label, method, ver, n)
+    
+    print("applying %s using n_components = %s" % (method, usen))
+    model = PCA(n_components=usen, random_state=seed)
+    Xt = model.fit_transform(X)
+
+    return Xt
+
+reconstructimages = False
+for i in [1, 2]:
+  print("="*10)
+  X, y, label, n_components_range = getDataset(i)
+  Xt = reduceDim(label, method, X, n_components_range, reconstructimages)
+  saveXt(label, method, Xt)
+  reconstructimages = True
